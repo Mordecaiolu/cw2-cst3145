d1d3933e4687d090b0ac848bb39af0f3d5423ae7
Tomisin <Tomisin@gmail.com>
2021-06-04 11:18:30 -0700

    server.js

---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express')
+const { ObjectID } = require('mongodb')
+// const bodyParser = require('body-parser')
+
+// Create an Express.js instance:
+const app = express()
+
+app.use(express.json())
+app.set('port', 3000)
+app.use ((req,res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+})
+
+// connect to MongoDB
+
+const MongoClient = require('mongodb').MongoClient;
+
+let db;
+
+MongoClient.connect('mongodb+srv://Mordecai:tomisin.@cluster0.j9uqx.mongodb.net/test', (err, client) => {
+
+    db = client.db('school')
+
+})
+
+// display a message for root path to show that API is working
+app.get('/', (req, res, next) => {
+    res.send('select a collection, e.g., /collection/messages')
+})
+
+// get the collection name
+app.param('collectionName', (req, res, next, collectionName) => {
+    req.collection = db.collection(collectionName)
+    // console.log('collection name:', req.collection)
+    return next()
+})
+
+// retrive all the objects from a collection
+app.get('/collection/:collectionName', (req, res, next) => {
+    req.collection.find({}).toArray((e, results) => {
+        if (e) return next(e);
+         res.send(results)
+    })
+})
+// post
+app.post('/collection/:collectionName', (req, res, next) => {
+    req.collection.insert(req.body, (e, results) => {
+        if (e) return next(e)
+        res.send(results.ops)
+    })
+})
+// put
+app.put('/collection/:collectionName/:id', (req, res, next) => {
+    req. collection.update(
+        {_id: new ObjectID(req.params.id)},
+        {$set: req.body},
+        {safe: true, multi: false},
+        (e, result) => {
+            if (e) return next(e)
+            res.send((result.result.n === 1) ? {msg: 'success'} : {msg: 'error'})
+        })
+})
+
+app.delete('/collection/:collectionName/:id', (req, res, next) => {
+    req.collection.deleteOne(
+        {_id: ObjectID(req.params.id) },(e, result) => {
+            if (e) return next(e)
+            res.send((result.result.n === 1) ?
+            {msg: 'success'} : {msg: 'error'})
+        })
+})
+
+const port = process.env.PORT || 3000
+app.listen(port)
